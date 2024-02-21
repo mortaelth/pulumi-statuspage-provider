0172c13771cf1067cf7bbe7f5f7898b05a9004b6
j.soukal <j.soukal@quadient.com>
2024-02-21 11:52:49 +0100

    tf module name fix

--- provider/resources.go
+++ provider/resources.go
@@ -16,7 +16,7 @@ package statuspage
 
 import (
 	"path/filepath"
-	"github.com/yannh/terraform-provider-statuspage"
+	"github.com/yannh/terraform-provider-statuspage/statuspage"
 	"github.com/pulumi/pulumi-terraform-bridge/v3/pkg/tfbridge"
 	shim "github.com/pulumi/pulumi-terraform-bridge/v3/pkg/tfshim"
 	shimv2 "github.com/pulumi/pulumi-terraform-bridge/v3/pkg/tfshim/sdk-v2"
